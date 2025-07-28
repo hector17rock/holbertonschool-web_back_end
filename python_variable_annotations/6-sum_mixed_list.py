bfbc29ac0314c532ffd1c902874f6f737ef68c27
Hector Soto <hector_17rock@hotmail.com>
2025-07-28 12:10:21 -0400

    TASK6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Module that defines a function to sum a list of intergers and floats."""
+"""Module that defines a function to sum a list of integers and floats."""
 
 
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[union[int, float]]) -> float:
-    """Returns the sum of a list of intergers and floats as a float."""
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of a list of integers and floats as a float."""
     return sum(mxd_lst)
