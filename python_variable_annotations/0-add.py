ce4722417ea83b347457719c3e8f163cef0b8210
Hector Soto <hector_17rock@hotmail.com>
2025-07-28 11:40:00 -0400

    TASK0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Module 0-add
+This module provides a function `add` that returns the sum of two float numbers.
+"""
 def add(a: float, b: float) -> float:
-    """RETURNS THe sum of two float numbers"""
+    """Returns the sum of two float numbers"""
     return a + b
