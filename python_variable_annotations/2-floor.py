a166fec9eabbe46130412af3a21575dffc154ace
Hector Soto <hector_17rock@hotmail.com>
2025-07-28 11:48:02 -0400

    TASK2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that defines a type-annotated floor n."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n."""
+    return math.floor(n)
