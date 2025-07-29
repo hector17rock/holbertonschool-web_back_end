2ace02f257b57ce7f26b2ef3a9d59740762ab92b
Hector Soto <hector_17rock@hotmail.com>
2025-07-29 12:13:54 -0400

    TASK0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,10 +10,10 @@ import random
 async def async_generator():
     """
     Async generator that yields random numbers between 0 and 10.
-    
+
     Loops 10 times, waiting 1 second each time before yielding
     a random float between 0 and 10.
-    
+
     Yields:
         float: Random number between 0 and 10
     """
