d832e5057fc1ebe16047a7e3d6fac92640b7b99a
Hector Soto <hector_17rock@hotmail.com>
2025-08-20 13:56:01 -0400

    TASK0

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    // Customize rules as needed
+  },
+};
+
