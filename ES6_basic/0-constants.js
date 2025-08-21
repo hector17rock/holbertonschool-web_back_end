955ffbf6ccb8d118184d4a1d816506e1f1905d39
Hector Soto <hector_17rock@hotmail.com>
2025-08-21 11:50:15 -0400

    CORRECTIONS

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -5,14 +5,9 @@ export function taskFirst() {
   return task;
 }
 
-export function getLast() {
-  return ' is okay';
-}
-
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast();
-
+  combination += ' is okay';
   return combination;
 }
 
