d9e34a14789a9dfce157783263193dfb36142392
Hector Soto <hector_17rock@hotmail.com>
2025-08-21 13:59:29 -0400

    CORRECTIONS

--- ES6_basic/eslint.config.js
+++ ES6_basic/eslint.config.js
@@ -1,17 +1,37 @@
 export default [
   {
+    files: ['**/*.js'],
+    ignores: ['**/node_modules/**', '**/*.config.js'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
-        console: "readonly",
-        process: "readonly"
+        console: 'readonly',
+        process: 'readonly'
       }
     },
     rules: {
-      "no-var": "error",
-      "prefer-const": "error",
-      "no-unused-vars": "warn"
+      // ES6+ rules
+      'no-var': 'error',
+      'prefer-const': 'error',
+      
+      // Code style rules
+      'indent': ['error', 2],
+      'semi': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      
+      // Variable and function rules
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-redeclare': 'error',
+      
+      // Best practices
+      'no-console': 'off',
+      'no-debugger': 'error',
+      'curly': 'error',
+      'eqeqeq': 'error',
+      'no-eval': 'error'
     }
   }
 ];
