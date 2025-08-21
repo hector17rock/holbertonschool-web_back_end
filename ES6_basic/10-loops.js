9a7171ba95498ecaaf2a0549ce432e98dd9495e7
Hector Soto <hector_17rock@hotmail.com>
2025-08-21 11:41:02 -0400

    TASK10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = appendString + array[i];
+  }
+  return array;
+}
---
+++ ES6_basic/10-main.js
@@ -0,0 +1,5 @@
+// 10-main.js
+import appendToEachArrayValue from './10-loops.js';
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+
