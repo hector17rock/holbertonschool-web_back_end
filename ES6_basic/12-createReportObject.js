daaace9b26cf1186fb172a06761b7b5da9ea63de
Hector Soto <hector_17rock@hotmail.com>
2025-08-21 11:44:35 -0400

    TASK12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+// 12-createReportObject.js
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
+
