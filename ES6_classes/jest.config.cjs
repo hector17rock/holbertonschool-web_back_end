49aca25c9dad8036b4690aaeb33bae44a3af2427
Hector Soto <hector_17rock@hotmail.com>
2025-08-22 12:03:51 -0400

    TASK0

--- ES6_classes/jest.config.cjs
+++ ES6_classes/jest.config.cjs
@@ -1,15 +1,4 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: 'node',
-  
-  // Use babel-jest for transforms to handle ES modules
-  transform: {
-    '^.+\.(js|jsx)$': 'babel-jest'
-  },
-  
-  // Ensure we can find test files
-  testMatch: ['**/*.(test|spec).js'],
-  
-  // Ignore node_modules transforms
-  transformIgnorePatterns: ['/node_modules/']
+  testEnvironment: 'node'
 };
