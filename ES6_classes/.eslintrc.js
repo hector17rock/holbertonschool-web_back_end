0ea92c53a65cb502f2041c7cf9a287b5f0581f32
Hector Soto <hector_17rock@hotmail.com>
2025-08-22 11:41:06 -0400

    CORRECTIONS

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,18 +1,38 @@
+
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    browser: false,
+    es6: true,
     jest: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    // add your custom rules here
-    'no-unused-vars': 'warn',
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
     'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
