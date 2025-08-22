ae35c20e011698b1e6ea5df91f9a740e6342bb65
Hector Soto <hector_17rock@hotmail.com>
2025-08-22 11:57:30 -0400

    TASK0

---
+++ ES6_classes/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'node',
+  // Explicitly unset testRunner to use Jest's default
+  testRunner: undefined
+};
+
+export default config;
