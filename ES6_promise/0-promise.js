d3846d24102299ce7a301b9d40abbe97cba0f738
Hector Soto <hector_17rock@hotmail.com>
2025-08-27 12:16:14 -0400

    TASK0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // This is a basic Promise that can be resolved or rejected
+    // For this exercise, we just need to return a Promise instance
+    resolve();
+  });
+}
