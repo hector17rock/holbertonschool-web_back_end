b352f506395df670f05206fb6a768f34060faa09
Hector Soto <hector_17rock@hotmail.com>
2025-08-27 12:37:05 -0400

    TASK06

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,16 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ]);
+
+  return results.map((res) => {
+    if (res.status === 'fulfilled') {
+      return { status: res.status, value: res.value };
+    }
+    return { status: res.status, value: res.reason.toString() };
+  });
 }
