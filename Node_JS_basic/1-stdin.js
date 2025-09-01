04122e3b5c8893c164c48b1fe0763345ab1cfe62
Hector Soto <hector_17rock@hotmail.com>
2025-09-01 14:40:41 -0400

    TASK01

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,12 @@
-const process = require('process');
-
-// Display welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Make stdin readable
 process.stdin.setEncoding('utf8');
 
-// Handle data input
 process.stdin.on('data', (data) => {
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-// Handle end of input (when user presses Ctrl+D or when piped input ends)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
