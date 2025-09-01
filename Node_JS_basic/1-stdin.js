af735c2ad6ec7bc9db0a1189e2b633a8faecad77
Hector Soto <hector_17rock@hotmail.com>
2025-09-01 14:48:58 -0400

    TASK01

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// 1-stdin.js
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
+
