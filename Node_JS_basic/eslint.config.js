ded029e4507c8b1c10a8015cb9cd75cf07a16603
Hector Soto <hector_17rock@hotmail.com>
2025-09-01 14:09:58 -0400

    INSTALLED DEPENDENCIES

---
+++ Node_JS_basic/eslint.config.js
@@ -0,0 +1,64 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        
+        // Node.js globals
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        
+        // Jest globals
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        jest: 'readonly'
+      }
+    },
+    rules: {
+      // Basic error prevention
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-redeclare': 'error',
+      'no-unreachable': 'error',
+      
+      // Code style rules
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error',
+      
+      // Best practices
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      
+      // ES6+ specific
+      'arrow-spacing': 'error',
+      'object-shorthand': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+      
+      // Class-specific rules (relevant for ES6 classes)
+      'no-dupe-class-members': 'error',
+      'no-useless-constructor': 'error'
+    }
+  }
+];
