af0ad6f884dbdada4fb0b3c3c1a27ec1b05ec506
Hector Soto <hector_17rock@hotmail.com>
2025-09-02 15:12:08 -0400

    TASK01

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,17 @@
-// 1-stdin.js
-console.log('Welcome to Holberton School, what is your name?');
+const { spawn } = require("child_process");
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+process.stdout.write("Welcome to Holberton School, what is your name? \n");
+process.stdin.on("data", (input) => {
+  const name = input.toString().trim();
+  process.stdout.write(`Your name is: ${name} \n`);
+  //const child = spawn("echo", ["This important software is now closing"]);
+  //child.stdout.pipe(process.stdout);
+  //child.on("close", () => {
+//    process.exit(0);
+//  });
 });
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.on("exit", () => {
+  console.log("This important software is now closing");
 });
 
