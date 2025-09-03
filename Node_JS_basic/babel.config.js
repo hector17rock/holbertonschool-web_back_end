86f77fa7aed188e65f0fbdffa534dd8df0f5442a
Hector Soto <hector_17rock@hotmail.com>
2025-09-03 15:31:15 -0400

    TASK01

--- Node_JS_basic/babel.config.js
+++ Node_JS_basic/babel.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
+  presets: ['env'],
 };
 
